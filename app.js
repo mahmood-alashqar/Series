cca4bbd3e626ff4c0061591d1753df15d784fc93
mahmood-alashqar <mahmood.ashqur@hotmail.com>
2021-03-08 13:00:38 +0200

    CSS test by mahmood alashqar

---
+++ app.js
@@ -0,0 +1,14 @@
+function tempFun()
+{
+ for (var i=0;i<3;i++)
+{
+    document.write('<br>'+"test function"+'<br>');
+}   
+}
+var temp2Fun=function ()
+{
+    document.write("Welcom To The Energy Drink Store!");
+}
+
+tempFun(); 
+temp2Fun();
